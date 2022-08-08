0d353a0bd591416f377f93552c2417430019b95b
Carsoboss <carson.karren@gmail.com>
2022-08-08 11:55:15 -0600

    first commit

---
+++ comps/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+<footer>
+Copyright 2001 Ninja list
+</footer>
+
+    );
+}
+
+export default Footer;
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Navbar from '../comps/Navbar'
+import Footer from '../comps/Footer'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Ninja List | Home</title>
+      <meta name="keywords" content="ninjas"/>
+    </Head>
+    <div>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <Link href="/ninjas">
+        <a className={styles.btn}>See Ninja Listing</a>
+    </Link>
+  </div>
+  </>
+  )
+}
